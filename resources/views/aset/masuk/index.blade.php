72d9e88286e1c8ae85dc0af5fbfd264d0e350050
imamnurhy <imamnurhy@gmail.com>
2020-09-21 13:22:09 +0700

    resolve error print pdf

--- resources/views/aset/masuk/index.blade.php
+++ resources/views/aset/masuk/index.blade.php
@@ -113,7 +113,7 @@ var table = $('#asetmasuk-table').dataTable({
                 extend: 'pdfHtml5',
                 text: 'PDF',
                 exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5, 7, 8, 9]
+                    columns: [ 0, 1, 2, 3, 4, 5, 6]
                 }
             },
         ],
